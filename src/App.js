acad6bd88a8e10a37280cee975eac380ab32c702
Jason Bobich <jasonbobich@gmail.com>
2018-05-25 16:35:34 -0600

    We don't need the whole frontstreet core library for this.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import './assets/css/frontstreet-core.css';
 import './assets/scss/main.scss'; // Must come before components.
 import Controls from './components/Controls';
 import Timer from './components/Timer';
